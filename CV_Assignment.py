8cb6d80072c113c59d94676556c44a47dd29d613
Keran Zhao <kzhao18@sheffield.ac.uk>
2024-05-23 21:29:07 +0100

    image stitch

--- CV_Assignment.py
+++ CV_Assignment.py
@@ -1,7 +1,7 @@
-# import numpy as np
-# import cv2 as cv
-# import matplotlib.pyplot as plt
-#
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
 # # filename = 'left.png'
 # # img = cv.imread(filename)
 # # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -46,35 +46,35 @@
 # # cv.waitKey(0)
 # # cv.destroyAllWindows()
 #
-# img1 = cv.imread('left.png')  # queryImage
-# img2 = cv.imread('right.png')  # trainImage
-#
-# # Initiate SIFT detector
-# sift = cv.SIFT_create()
-#
-# # find the keypoints and descriptors with SIFT
-# kp1, des1 = sift.detectAndCompute(img1, None)
-# kp2, des2 = sift.detectAndCompute(img2, None)
-#
-# # BFMatcher with default params
-# bf = cv.BFMatcher(cv.NORM_L2, crossCheck=False)
+img1 = cv.imread('left.png')  # queryImage
+img2 = cv.imread('right.png')  # trainImage
+
+# Initiate SIFT detector
+sift = cv.SIFT_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1, None)
+kp2, des2 = sift.detectAndCompute(img2, None)
+
+# BFMatcher with default params
+bf = cv.BFMatcher(cv.NORM_L2, crossCheck=False)
 # # matches1 = bf.knnMatch(des1, des2, k = 1)
-# matches2 = bf.knnMatch(des1, des2, k=2)
+matches2 = bf.knnMatch(des1, des2, k=2)
 #
-# # Apply ratio test
-# good = []
-# for m, n in matches2:
-#     m_ssd = m.distance ** 2
-#     n_ssd = n.distance ** 2
-#     if m_ssd < 0.1 * n_ssd:
-#         good.append([m])
-#
+# Apply ratio test
+good = []
+for m, n in matches2:
+    m_ssd = m.distance ** 2
+    n_ssd = n.distance ** 2
+    if m_ssd < 0.75 * n_ssd:
+        good.append([m])
+
 # # cv.drawMatchesKnn expects list of lists as matches.
 # # img2 = cv.drawMatchesKnn(img1, kp1, img2, kp2, matches1, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-# img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 #
 # # 保存匹配结果图片
-# cv.imwrite('ssd_ratio_matched_result.png', img3)
+cv.imwrite('ssd_ratio_matched_result.png', img3)
 # # cv.imwrite('ssd_matched_result.png', img2)
 #
 #
@@ -151,145 +151,145 @@ import numpy as np
 import imutils
 import cv2
 
-class Stitcher:
-    def __init__(self):
-        # determine if we are using OpenCV v3.X
-        self.isv3 = imutils.is_cv3(or_better=True)
-
-    def stitch(self, images, ratio=0.75, reprojThresh=4.0, showMatches=False):
-        # unpack the images, then detect keypoints and extract
-        # local invariant descriptors from them
-        (imageB, imageA) = images
-        (kpsA, featuresA) = self.detectAndDescribe(imageA)
-        (kpsB, featuresB) = self.detectAndDescribe(imageB)
-
-        # match features between the two images
-        M = self.matchKeypoints(kpsA, kpsB, featuresA, featuresB, ratio, reprojThresh)
-
-        # if the match is None, then there aren't enough matched
-        # keypoints to create a panorama
-        if M is None:
-            return None
-
-        # otherwise, apply a perspective warp to stitch the images
-        # together
-        (matches, H, status) = M
-        result = cv2.warpPerspective(imageA, H, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
-        result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
-
-        # check to see if the keypoint matches should be visualized
-        if showMatches:
-            vis = self.drawMatches(imageA, imageB, kpsA, kpsB, matches, status)
-
-            # return a tuple of the stitched image and the
-            # visualization
-            return (result, vis)
-
-        # return the stitched image
-        return result
-
-    def detectAndDescribe(self, image):
-        # convert the image to grayscale
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # check to see if we are using OpenCV 3.X
-        if self.isv3:
-            # detect and extract features from the image
-            descriptor = cv2.SIFT_create()
-            (kps, features) = descriptor.detectAndCompute(image, None)
-        # otherwise, we are using OpenCV 2.4.X
-        else:
-            # detect keypoints in the image
-            detector = cv2.FeatureDetector_create("SIFT")
-            kps = detector.detect(gray)
-
-            # extract features from the image
-            extractor = cv2.DescriptorExtractor_create("SIFT")
-            (kps, features) = extractor.compute(gray, kps)
-
-        # convert the keypoints from KeyPoint objects to NumPy
-        # arrays
-        kps = np.float32([kp.pt for kp in kps])
-
-        # return a tuple of keypoints and features
-        return (kps, features)
-
-    def matchKeypoints(self, kpsA, kpsB, featuresA, featuresB, ratio, reprojThresh):
-        # compute the raw matches and initialize the list of actual
-        # matches
-        matcher = cv2.DescriptorMatcher_create("BruteForce")
-        rawMatches = matcher.knnMatch(featuresA, featuresB, 2)
-        matches = []
-
-        # loop over the raw matches
-        for m in rawMatches:
-            # ensure the distance is within a certain ratio of each
-            # other (i.e. Lowe's ratio test)
-            if len(m) == 2 and m[0].distance < m[1].distance * ratio:
-                matches.append((m[0].trainIdx, m[0].queryIdx))
-
-        # computing a homography requires at least 4 matches
-        if len(matches) > 4:
-            # construct the two sets of points
-            ptsA = np.float32([kpsA[i] for (_, i) in matches])
-            ptsB = np.float32([kpsB[i] for (i, _) in matches])
-
-            # compute the homography between the two sets of points
-            (H, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC, reprojThresh)
-
-            # return the matches along with the homography matrix
-            # and status of each matched point
-            return (matches, H, status)
-
-        # otherwise, no homography could be computed
-        return None
-
-    def drawMatches(self, imageA, imageB, kpsA, kpsB, matches, status):
-        # initialize the output visualization image
-        (hA, wA) = imageA.shape[:2]
-        (hB, wB) = imageB.shape[:2]
-        vis = np.zeros((max(hA, hB), wA + wB, 3), dtype="uint8")
-        vis[0:hA, 0:wA] = imageA
-        vis[0:hB, wA:] = imageB
-
-        # loop over the matches
-        for ((trainIdx, queryIdx), s) in zip(matches, status):
-            # only process the match if the keypoint was successfully
-            # matched
-            if s == 1:
-                # draw the match
-                ptA = (int(kpsA[queryIdx][0]), int(kpsA[queryIdx][1]))
-                ptB = (int(kpsB[trainIdx][0]) + wA, int(kpsB[trainIdx][1]))
-                cv2.line(vis, ptA, ptB, (0, 255, 0), 1)
-
-        # return the visualization
-        return vis
-
-# 从命令行参数获取图像路径
-import argparse
-import imutils
-import cv2
-
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--first", required=True, help="path to the first image")
-ap.add_argument("-s", "--second", required=True, help="path to the second image")
-args = vars(ap.parse_args())
-
-# load the two images and resize them to have a width of 400 pixels
-# (for faster processing)
-imageA = cv2.imread(args["first"])
-imageB = cv2.imread(args["second"])
-imageA = imutils.resize(imageA, width=400)
-imageB = imutils.resize(imageB, width=400)
-
-# stitch the images together to create a panorama
-stitcher = Stitcher()
-(result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
-
-# show the images
-cv2.imshow("Image A", imageA)
-cv2.imshow("Image B", imageB)
-cv2.imshow("Keypoint Matches", vis)
-cv2.imshow("Result", result)
-cv2.waitKey(0)
+# class Stitcher:
+#     def __init__(self):
+#         # determine if we are using OpenCV v3.X
+#         self.isv3 = imutils.is_cv3(or_better=True)
+#
+#     def stitch(self, images, ratio=0.75, reprojThresh=4.0, showMatches=False):
+#         # unpack the images, then detect keypoints and extract
+#         # local invariant descriptors from them
+#         (imageB, imageA) = images
+#         (kpsA, featuresA) = self.detectAndDescribe(imageA)
+#         (kpsB, featuresB) = self.detectAndDescribe(imageB)
+#
+#         # match features between the two images
+#         M = self.matchKeypoints(kpsA, kpsB, featuresA, featuresB, ratio, reprojThresh)
+#
+#         # if the match is None, then there aren't enough matched
+#         # keypoints to create a panorama
+#         if M is None:
+#             return None
+#
+#         # otherwise, apply a perspective warp to stitch the images
+#         # together
+#         (matches, H, status) = M
+#         result = cv2.warpPerspective(imageA, H, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
+#         result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
+#
+#         # check to see if the keypoint matches should be visualized
+#         if showMatches:
+#             vis = self.drawMatches(imageA, imageB, kpsA, kpsB, matches, status)
+#
+#             # return a tuple of the stitched image and the
+#             # visualization
+#             return (result, vis)
+#
+#         # return the stitched image
+#         return result
+#
+#     def detectAndDescribe(self, image):
+#         # convert the image to grayscale
+#         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#
+#         # check to see if we are using OpenCV 3.X
+#         if self.isv3:
+#             # detect and extract features from the image
+#             descriptor = cv2.SIFT_create()
+#             (kps, features) = descriptor.detectAndCompute(image, None)
+#         # otherwise, we are using OpenCV 2.4.X
+#         else:
+#             # detect keypoints in the image
+#             detector = cv2.FeatureDetector_create("SIFT")
+#             kps = detector.detect(gray)
+#
+#             # extract features from the image
+#             extractor = cv2.DescriptorExtractor_create("SIFT")
+#             (kps, features) = extractor.compute(gray, kps)
+#
+#         # convert the keypoints from KeyPoint objects to NumPy
+#         # arrays
+#         kps = np.float32([kp.pt for kp in kps])
+#
+#         # return a tuple of keypoints and features
+#         return (kps, features)
+#
+#     def matchKeypoints(self, kpsA, kpsB, featuresA, featuresB, ratio, reprojThresh):
+#         # compute the raw matches and initialize the list of actual
+#         # matches
+#         matcher = cv2.DescriptorMatcher_create("BruteForce")
+#         rawMatches = matcher.knnMatch(featuresA, featuresB, 2)
+#         matches = []
+#
+#         # loop over the raw matches
+#         for m in rawMatches:
+#             # ensure the distance is within a certain ratio of each
+#             # other (i.e. Lowe's ratio test)
+#             if len(m) == 2 and m[0].distance < m[1].distance * ratio:
+#                 matches.append((m[0].trainIdx, m[0].queryIdx))
+#
+#         # computing a homography requires at least 4 matches
+#         if len(matches) > 4:
+#             # construct the two sets of points
+#             ptsA = np.float32([kpsA[i] for (_, i) in matches])
+#             ptsB = np.float32([kpsB[i] for (i, _) in matches])
+#
+#             # compute the homography between the two sets of points
+#             (H, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC, reprojThresh)
+#
+#             # return the matches along with the homography matrix
+#             # and status of each matched point
+#             return (matches, H, status)
+#
+#         # otherwise, no homography could be computed
+#         return None
+#
+#     def drawMatches(self, imageA, imageB, kpsA, kpsB, matches, status):
+#         # initialize the output visualization image
+#         (hA, wA) = imageA.shape[:2]
+#         (hB, wB) = imageB.shape[:2]
+#         vis = np.zeros((max(hA, hB), wA + wB, 3), dtype="uint8")
+#         vis[0:hA, 0:wA] = imageA
+#         vis[0:hB, wA:] = imageB
+#
+#         # loop over the matches
+#         for ((trainIdx, queryIdx), s) in zip(matches, status):
+#             # only process the match if the keypoint was successfully
+#             # matched
+#             if s == 1:
+#                 # draw the match
+#                 ptA = (int(kpsA[queryIdx][0]), int(kpsA[queryIdx][1]))
+#                 ptB = (int(kpsB[trainIdx][0]) + wA, int(kpsB[trainIdx][1]))
+#                 cv2.line(vis, ptA, ptB, (0, 255, 0), 1)
+#
+#         # return the visualization
+#         return vis
+#
+# # 从命令行参数获取图像路径
+# import argparse
+# import imutils
+# import cv2
+#
+# # construct the argument parse and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-f", "--first", required=True, help="path to the first image")
+# ap.add_argument("-s", "--second", required=True, help="path to the second image")
+# args = vars(ap.parse_args())
+#
+# # load the two images and resize them to have a width of 400 pixels
+# # (for faster processing)
+# imageA = cv2.imread(args["first"])
+# imageB = cv2.imread(args["second"])
+# imageA = imutils.resize(imageA, width=400)
+# imageB = imutils.resize(imageB, width=400)
+#
+# # stitch the images together to create a panorama
+# stitcher = Stitcher()
+# (result, vis) = stitcher.stitch([imageA, imageB], showMatches=False)
+#
+# # show the images
+# cv2.imshow("Image A", imageA)
+# cv2.imshow("Image B", imageB)
+# cv2.imshow("Keypoint Matches", vis)
+# cv2.imshow("Result", result)
+# cv2.waitKey(0)
--- computer_vision_assignment.py
+++ computer_vision_assignment.py
@@ -23,12 +23,81 @@ class Sticher:
         # 初始化SIFT检测器
         sift = cv.SIFT_create()
         # 使用SIFT检测关键点和描述符
-        kp, des = sift.detectAndCompute(gray, None)
+        keypoints, descriptors = sift.detectAndCompute(gray, None)
         # 绘制特征点
-        image_with_keypoints = cv.drawKeypoints(image, kp, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        keypoints_info = [(kp.pt, kp.angle) for kp in kp]
+        image_with_keypoints = cv.drawKeypoints(image, keypoints, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        keypoints_info = [(kp.pt, kp.angle) for kp in keypoints]
 
-        return image_with_keypoints, keypoints_info
+        return image_with_keypoints, keypoints, keypoints_info, descriptors
+
+    def ORB_points_dector(self, image):
+        # 将图像转换为灰度图像
+        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        orb = cv.ORB_create()
+        keypoints, descriptors = orb.detectAndCompute(gray, None)
+        image_with_keypoints = cv.drawKeypoints(image, keypoints, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        return image_with_keypoints, keypoints, descriptors
+
+    def compute_ssd(self, descriptor1, descriptor2):
+        ssd = (np.linalg.norm(descriptor1 - descriptor2)) ** 2
+        return ssd
+
+    def match_descriptors_ssd(self, descriptors1, descriptors2):
+        bf = cv.BFMatcher(cv.NORM_L2, crossCheck=False)
+        matches = bf.match(descriptors1, descriptors2)
+        matches = sorted(matches, key=lambda x: x.distance**2)
+        return matches[:800]
+
+    def ratio_test(self, descriptors1, descriptors2):
+        """
+        应用比率测试筛选匹配对
+        :param matches: 所有匹配对的索引和距离
+        :param ratio: 比率测试阈值
+        :return: 通过比率测试的匹配对
+        """
+        bf = cv.BFMatcher(cv.NORM_L2, crossCheck=False)
+        matches = bf.knnMatch(descriptors1, descriptors2, k=2)
+        good_matches = []
+        for m, n in matches:
+            m_ssd = m.distance ** 2
+            n_ssd = n.distance ** 2
+            if m_ssd < 0.75 * n_ssd:
+                good_matches.append(m)
+        return good_matches
+
+    def stitch_images(self, images):
+        (imageB, imageA) = images
+        # 使用SIFT特征检测和描述
+        _, keypointsA, _, descriptorsA = self.SIFT_points_dector(imageA)
+        _, keypointsB, _, descriptorsB = self.SIFT_points_dector(imageB)
+
+        # 使用比率测试进行特征点匹配
+        matches = self.ratio_test(descriptorsA, descriptorsB)
+
+        src_pts = np.float32([keypointsA[m.queryIdx].pt for m in matches])
+        dst_pts = np.float32([keypointsB[m.trainIdx].pt for m in matches])
+
+        # 计算单应性矩阵
+        H, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+
+        # # 使用单应性矩阵变换图像
+        # heightA, widthA = imageA.shape[:2]
+        # heightB, widthB = imageB.shape[:2]
+        #
+        # # 计算变换后图像的尺寸
+        # result_width = widthA + widthB
+        # result_height = max(heightA, heightB)
+        #
+        # # 变换图像A
+        # result = cv.warpPerspective(imageA, H, (result_width, result_height))
+        #
+        # # # 将图像B拷贝到结果图像中
+        # # result[0:heightB, 0:widthB] = imageB
+        result = cv.warpPerspective(imageA, H, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
+        result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
+
+        return result
+
 
 
 ap = argparse.ArgumentParser()
@@ -43,13 +112,24 @@ imageB = cv.imread(args["second"])
 stitcher = Sticher()
 imageA_corners, imageA_harried = stitcher.harries_corner_dector(imageA.copy())
 imageB_corners, imageB_harried = stitcher.harries_corner_dector(imageB.copy())
-imageA_SIFT, imageA_SIFT_info = stitcher.SIFT_points_dector(imageA.copy())
-imageB_SIFT, imageB_SIFT_info = stitcher.SIFT_points_dector(imageB.copy())
-
+imageA_SIFT, imageA_SIFT_keypoints, imageA_SIFT_info, imageA_SIFT_descriptors = stitcher.SIFT_points_dector(imageA.copy())
+imageB_SIFT, imageB_SIFT_keypoints, imageB_SIFT_info, imageB_SIFT_descriptors = stitcher.SIFT_points_dector(imageB.copy())
+imageA_ORB, imageA_ORB_keypoints, imageA_ORB_descriptors = stitcher.ORB_points_dector(imageA.copy())
+imageB_ORB, imageB_ORB_keypoints, imageB_ORB_descriptors = stitcher.ORB_points_dector(imageB.copy())
+SIFT_ssd_matches = stitcher.match_descriptors_ssd(imageA_SIFT_descriptors, imageB_SIFT_descriptors)
+SIFT_ssd_match_img = cv.drawMatches(imageA, imageA_SIFT_keypoints, imageB, imageB_SIFT_keypoints, SIFT_ssd_matches, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+SIFT_ratio_matches = stitcher.ratio_test(imageA_SIFT_descriptors, imageB_SIFT_descriptors)
+SIFT_ratio_match_img = cv.drawMatches(imageA, imageA_SIFT_keypoints, imageB, imageB_SIFT_keypoints, SIFT_ratio_matches, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+stitched_image = stitcher.stitch_images([imageA, imageB])
 cv.imshow("Image A", imageA_harried)
 cv.imshow("Image B", imageB_harried)
 cv.imshow("Image A SIFT", imageA_SIFT)
 cv.imshow("Image B SIFT", imageB_SIFT)
+cv.imshow("Image A ORB", imageA_ORB)
+cv.imshow("SIFT_ssd_match_img", SIFT_ssd_match_img)
+cv.imshow("SIFT_ratio_match_img", SIFT_ratio_match_img)
+cv.imshow("stitched_image ", stitched_image)
+
 cv.waitKey(0)
 cv.destroyAllWindows()
 
